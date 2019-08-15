ac1a63bc211f0b761a5d1cf37a5ddbfad10d57eb
ylone3 <46116903+ylone3@users.noreply.github.com>
2019-08-15 18:14:03 +0800

    tomcat启动类修改

--- spring-cloud-server/src/main/java/com/hexun/busi/config/springcloudserver/SpringCloudServerApplication.java
+++ spring-cloud-server/src/main/java/com/hexun/busi/config/springcloudserver/SpringCloudServerApplication.java
@@ -2,11 +2,18 @@ package com.hexun.busi.config.springcloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class SpringCloudServerApplication {
+public class SpringCloudServerApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringCloudServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudServerApplication.class, args);
